d90b4f634df73511c461515e5afb125ca677fb3d
drnaline <>
2020-03-20 16:37:19 -0700

    Added player DAO

--- GameJDBCProjectStudent/src/cs4347/jdbcGame/dao/impl/PlayerDAOImpl.java
+++ GameJDBCProjectStudent/src/cs4347/jdbcGame/dao/impl/PlayerDAOImpl.java
@@ -15,6 +15,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -63,7 +64,7 @@ public class PlayerDAOImpl implements PlayerDAO
     public Player retrieve(Connection connection, Long playerID) throws SQLException, DAOException
     {
     	 if (playerID == null) {
-             throw new DAOException("Trying to retrieve Game with NULL ID");
+             throw new DAOException("Trying to retrieve Player with NULL ID");
          }
 
          PreparedStatement ps = null;
@@ -83,35 +84,109 @@ public class PlayerDAOImpl implements PlayerDAO
                  ps.close();
              }
          }
-    	return null;
     }
     
-    
+    final static String updateSQL = "UPDATE player SET first_name = ?, last_name = ?, join_date = ?, email = ? WHERE id = ?;";
 
     @Override
     public int update(Connection connection, Player player) throws SQLException, DAOException
     {
-        
-    	return 0;
+    	Long id = player.getId();
+        if (id == null) {
+            throw new DAOException("Trying to update Player with NULL ID");
+        }
+
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(updateSQL);
+            ps.setString(1, player.getFirstName());
+            ps.setString(2, player.getLastName());
+            ps.setDate(3, new java.sql.Date(player.getJoinDate().getTime()));
+            ps.setString(4, player.getEmail());
+            ps.setLong(5, id);
+
+            int rows = ps.executeUpdate();
+            return rows;
+        }
+        finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+        }
+    	
     }
 
+    final static String deleteSQL = "delete from player where id = ?;";	
+    
     @Override
     public int delete(Connection connection, Long playerID) throws SQLException, DAOException
     {
-        return 0;
-    }
+    
+    	if (playerID == null) {
+             throw new DAOException("Trying to delete Player with NULL ID");
+         }
 
+         PreparedStatement ps = null;
+         try {
+             ps = connection.prepareStatement(deleteSQL);
+             ps.setLong(1, playerID);
+
+             int rows = ps.executeUpdate();
+             return rows;
+         }
+         finally {
+             if (ps != null && !ps.isClosed()) {
+                 ps.close();
+             }
+         }
+    }
+    
+    final static String countSQL = "select count(*) from player";
+    
     @Override
     public int count(Connection connection) throws SQLException, DAOException
     {
-        return 0;
+    	PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(countSQL);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                throw new DAOException("No Count Returned");
+            }
+            int count = rs.getInt(1);
+            return count;
+        }
+        finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+        }
     }
-
+    
+    final static String retrieveByJoinDateSQL = "select player.id,first_name,last_name,join_date,email from player where join_date between ? and ?";
+    
     @Override
     public List<Player> retrieveByJoinDate(Connection connection, Date start, Date end)
             throws SQLException, DAOException
     {
-        return null;
+    	List<Player> result = new ArrayList<Player>();
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement(retrieveByJoinDateSQL);
+            ps.setDate(1, new java.sql.Date(start.getTime()));
+            ps.setDate(2, new java.sql.Date(end.getTime()));
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Player player = extractFromRS(rs);
+                result.add(player);
+            }
+            return result;
+        }
+        finally {
+            if (ps != null && !ps.isClosed()) {
+                ps.close();
+            }
+        }
     }
     private Player extractFromRS(ResultSet rs) throws SQLException
     {
